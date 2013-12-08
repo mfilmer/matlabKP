431935698308012bcc8538c9339fd655ca2f9592
unknown <mjf1179@ASI14.main.ad.rit.edu>
2013-12-08 17:46:43 -0500

    Turns out makeSolveFunc isn't actually needed
    I just used a lambda function instead

--- egVsP.m
+++ egVsP.m
@@ -5,14 +5,16 @@ q = 1.602177e-19;       % [C] elementary charge
 pArray = logspace(log10(0.1), log10(1000), 100);
 arrayLen = length(pArray);  %yeah, I know it is hardcoded one line up
 equArray = cell(1,arrayLen);
-solveFunc = makeSolveFunc(1.1*pi, 2*pi);
-alphaAArray = cell(1,arrayLen);
+solveFunc = @(x) solve(x, 1.1*pi, 2*pi);
+%solveFunc = makeSolveFunc(1.1*pi, 2*pi);
 for i = 1:arrayLen
     equArray{i} = charEqu(pArray(i));
-    alphaAArray{i} = solveFunc(equArray{i});
 end
-
-alphaAArray = cell2mat(alphaAArray) - pi;
+alphaAArray = cellfun(solveFunc, equArray);
+alphaAArray = alphaAArray - pi;
 
 %plot stuff
-semilogy(alphaAArray);
+loglog(pArray, alphaAArray);
+set(gca, 'YTickLabel',num2str(get(gca,'YTick')'));
+set(gca, 'XTickLabel',num2str(get(gca,'XTick')'));
+set(gca, 'YLim', [4e-2, 4]);
--- makeSolveFunc.m
+++
@@ -1,5 +0,0 @@
-function sFunc = makeSolveFunc(start, stop)
-%MAKESOLVEFUNC return a function that solves an equation
-sFunc = @(func) solve(func, start, stop);
-end
-
