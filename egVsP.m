13dfd9f2fe3c91cec6aa6a2d918cc3338b62b190
unknown <mjf1179@ASI14.main.ad.rit.edu>
2013-12-08 18:13:49 -0500

    Basically works
    I can generate a curve for each band gap up to the nth gap. So far no
    problems, surprisingly. As expected, each band gap is smaller than the
    preceeding gap. This difference decreases for each subsequent gap.

--- egVsP.m
+++ egVsP.m
@@ -2,14 +2,26 @@ m_0 = 9.10938;          % [kg] mass of electron
 hbar = 1.054572e-34;    % [Js] reduced Planck constant
 q = 1.602177e-19;       % [C] elementary charge
 
+bandGap = cell(1,3);
+
 pArray = logspace(log10(0.1), log10(1000), 100);
 arrayLen = length(pArray);  %yeah, I know it is hardcoded one line up
-solveFunc = @(p) findBandEdge(p, 2, 'bottom');
-alphaAArray = arrayfun(solveFunc, pArray);
-alphaAArray = alphaAArray - pi;
+
+for i = 1:10
+    solveFunc = @(p) findBandEdge(p, i+1, 'bottom');
+    alphaAArray = arrayfun(solveFunc, pArray);
+    bandGap{i} = alphaAArray - pi*i;
+end
 
 %plot egVsP
-loglog(pArray, alphaAArray);
+loglog(pArray, bandGap{1})
+hold on;
+for i = 2:10
+    plot(pArray, bandGap{i});
+end
+hold off;
+set(gca, 'YLim', [4e-2, 4]);
 set(gca, 'YTickLabel',num2str(get(gca,'YTick')'));
 set(gca, 'XTickLabel',num2str(get(gca,'XTick')'));
-set(gca, 'YLim', [4e-2, 4]);
+xlabel('P')
+ylabel('Eg')
